8bb219526de3690b05953be95adfd740f0ba1003
Cole Snyder <MidLanerST@protonmail.com>
2023-02-02 23:52:03 -0700

    Set up initial project configuration

---
+++ docs/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    './_layouts/**/*.html'
+  ],
+}
